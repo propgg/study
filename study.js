a491922f4ec5014a921d7732d561c48acd42c79c
Lukas <lukas.lindqvist96@gmail.com>
2015-11-19 16:17:16 +0100

    templating working, basic math function added

--- study.js
+++ study.js
@@ -2,13 +2,6 @@ Problems = new Mongo.Collection("problems");
 Email = new Mongo.Collection("email");
 
 if (Meteor.isClient) {
-    var newTest = Email.find();
-
-    var newTest2 = newTest.fetch()[0].emailText;
-    
-    var html = Spacebars.toHTML({ username: 'lukas' }, newTest2);
-
-    console.log(html);
 
     Template.body.helpers({
         answers: function () {
@@ -20,6 +13,18 @@ if (Meteor.isClient) {
     Template.body.events({
         "submit .answer": function (event) {
             event.preventDefault();
+
+            var newTest = Email.find();
+
+            var i = newTest.fetch()[2];
+
+            var rnd = Math.floor((Math.random() * i.value1[1]) + i.value1[0]);
+
+            var html = Spacebars.toHTML({ value1 : rnd, value2 : i.value2}, i.emailText);
+
+            console.log(rnd + i.value2);
+
+            console.log(html);
 
             var answer = event.target.text.value;
 
